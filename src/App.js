54a741b230baf212244e251ddc1fbb6e124571b8
breakfasting <breakfast.ing@gmail.com>
2019-01-01 14:20:32 +0800

    Completed Search Function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DisplayList from './DisplayList';
+import Selected from './Selected';
 import { Container, Row, Col, Jumbotron, InputGroup, Input, Button } from 'reactstrap';
 import './App.css';
 
@@ -32,7 +33,7 @@ class App extends Component {
       <div className="App">
         <Container>
           <Jumbotron className="mt-5">
-            <h1 className="display-4">Custom Itinerary Planning</h1>
+            <h1 className="display-2">Custom Itinerary Planning</h1>
             <p>Please search and select the desired Point-of-interests</p>
           </Jumbotron>
           <Row className="justify-content-center">
@@ -47,11 +48,15 @@ class App extends Component {
               </InputGroup>
             </Col>
             <Col lg="10">
-              <h1>value is {this.state.filterText}</h1>
+              {/* <h1>value is {this.state.filterText}</h1> */}
               <DisplayList filterText={this.state.filterText} addPOI={this.addPOI} />
             </Col>
+            <Col lg="10" className="mt-5">
+              <h1 class="display-3">Selected POIs</h1>
+              <Selected />
+            </Col>
           </Row>
-
+          
 
         </Container>
         
--- src/DisplayList.js
+++ src/DisplayList.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText, Badge} from 'reactstrap';
 import displayData from './data/scenic_spot_C_f.json';
+
+function TopBar(props) {
+  if (props.results > 0) {
+    return (
+      <ListGroupItem color="success" >
+        Showing results for '{props.text == '' ? 'any' : props.text}', <Badge color="success">{props.results > 5 ? '1-5' : '1-' + props.results }</Badge> of a total of <Badge color="success">{props.results}</Badge> results.
+      </ListGroupItem>
+    );
+  }
+  return (
+    <ListGroupItem color="danger">
+      No results for '{props.text}'.
+    </ListGroupItem>
+  );
+}
 
 class DisplayList extends Component {
   render() {
@@ -12,10 +27,11 @@ class DisplayList extends Component {
     return (
       <div>
         <ListGroup className="text-left mt-1 shadow-sm">
+          <TopBar results={filtered.length} text={this.props.filterText} />
           {filtered.slice(0,5).map(
             elements => {
               return(
-                <ListGroupItem key={elements.Id} tag="a" href="#" action onClick = {(e) => this.props.addPOI(elements.Id)}>
+                <ListGroupItem key={elements.Id} tag="a" href="#" action onClick = {(e) => this.props.addPOI({id: elements.Id, name: elements.Name})}>
                   <ListGroupItemHeading>{elements.Name}</ListGroupItemHeading>
                   <ListGroupItemText className="text-truncate">
                     {elements.Toldescribe}
