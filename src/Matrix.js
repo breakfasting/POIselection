87ae001cf928c0fd1658c7c01cfebe7fc8a8988d
breakfasting <breakfast.ing@gmail.com>
2019-01-03 22:37:41 +0800

    Correctly listing props from app.js in matrix

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,14 +1,73 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Jumbotron, InputGroup, Input, Button } from 'reactstrap';
+import { Container, Row, Col, Jumbotron, InputGroup, Input, Button, Table } from 'reactstrap';
 
 class Matrix extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: [],
+    };
+  }
+
+  componentWillMount() {
+    if (this.props.location.state !== undefined) {
+      this.setState({
+        selected: this.props.location.state,
+      });
+    }
+  }
+
+  render() {
     return (
       <Container>
-        <h1>Adjacency Matrix</h1>
+        <Jumbotron className="mt-5 text-center">
+          <h1 className="display-2">Network Adjacency Matrix</h1>
+          <p>Displaying the static network adjacency matrix through querying google directions API</p>
+        </Jumbotron>
         <p>
-          {console.log(this.props.location.state)}
+          A total of {this.state.selected.length} POIs are selected.
         </p>
+        
+        <Table responsive bordered>
+          <thead>
+            <tr>
+              <th>#</th>
+                {this.state.selected.map( element => {
+                  return <th>{element.name}</th>;
+                } )}
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.selected.map( element => {
+              return (
+                <tr> 
+                  <th>{element.name}</th>
+                    {this.state.selected.map( element => {
+                      return <td>data</td>;
+                    } )}                  
+                </tr>
+              );
+            } )}
+            {/* <tr>
+              <th scope="row">台灣金屬創意館</th>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr> */}
+          </tbody>
+        </Table>
       </Container>
     );
   }
